25e4ada6b1f33e69370d8d71d8e5fc9bdc39cb68
Hunter Nguyen <hunter.nguyennnn@gmail.com>
2017-11-18 18:48:24 +0000

    2.0

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -18,7 +18,8 @@
 
         // container which will be returned
         var background;
-        
+         var backgroundBox;
+        var buildings = [];
         // add objects for display inb ackground
         // called at the start of game and whenever the page is resized
         function render() {
@@ -26,6 +27,7 @@
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
+           
 
             background.removeAllChildren();
 
@@ -33,8 +35,37 @@
 
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'black');
             background.addChild(backgroundFill);
+            
+            var galaxy = draw.bitmap('img/galaxypng.png');
+            galaxy.scaleX = 1;
+            galaxy.scaleY = .5;
+            background.addChild(galaxy);
+            
+            var saturn = draw.bitmap("http://vignette1.wikia.nocookie.net/clubpenguin/images/c/ce/Beta_Team_Solar_System_Saturn.png/revision/latest?cb=20130108130824.png");
+            saturn.x = 900;
+            saturn.y = 275;
+            saturn.scaleX = .28;
+            saturn.scaleY = .28;
+            background.addChild(saturn);
+            
+            backgroundBox = draw.rect(100,100,'red');
+            backgroundBox.x = 300;
+            backgroundBox.y = 350;
+           // background.addChild(backgroundBox);
+           
+           var buildingHeight = 300;
+            var building;
+            for(var i=0;i<5;++i) {
+                building = draw.rect(75,buildingHeight,'red','Black',1);
+                building.x = 200*i;
+                building.y = groundY-buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+}
+            
+            
         }
         
         // Perform background animation
@@ -44,6 +75,14 @@
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
+            
+            backgroundBox.x = backgroundBox.x - 1;
+            
+            
+            if(backgroundBox.x < -100) {
+            backgroundBox.x = canvasWidth;
+            }
+                    
 
         }
 
@@ -55,6 +94,8 @@
         app.addUpdateable(background);
         
         render();
+        
+
         return background;
     };
 }(window));
